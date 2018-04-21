3b753ca5ceb66774c68e4de1f444a7bf563f6b55
rophiat <rophiat13@gmail.com>
2018-04-21 19:57:43 +0000

    Created django movie web app

--- movies/views.py
+++ movies/views.py
@@ -20,30 +20,50 @@ def create(request):
     if request.method == 'POST':
         data = {
             'Name': request.POST.get('name'),
-            'Pictures': [{'url': request.POST.get('url')}],
+            'Pictures': [{'url': request.POST.get('url') or 'https://sl.d.umn.edu/och/PhotoGallery/no-image-available.jpg'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes')
         }
-
-        response = AT.insert(data)
-        messages.success(request, 'New movie added: {}'.format(response['fields'].get('Name')))
+        try:
+             response = AT.insert(data)
+        #notify on create
+             messages.success(request, 'New Movie Added: []'.format (respose['fields'].get('Name')))
+        except Exception as e:
+             messages.warning(request, 'Got an error when trying to create a movie: {}'.format (e))
     return redirect('/')
-
 
 def edit(request, movie_id):
     if request.method == 'POST':
-        data = {
+        data= {
             'Name': request.POST.get('name'),
-            'Pictures': [{'url': request.POST.get('url')}],
+            'Pictures': [{'url': request.POST.get('url') or 'https://sl.d.umn.edu/och/PhotoGallery/no-image-available.jpg'}],
             'Rating': int(request.POST.get('rating')),
             'Notes': request.POST.get('notes')
         }
-        response = AT.update(movie_id, data)
-        messages.success(request, 'Updated movie: {}'.format(response['fields'].get('Name')))
+        try:
+             response = AT.update(movie_id, data)
+
+        #notify on edit
+             messages.success(request, 'Movie edited: []'.format (response['fields'].get('Name')))
+        except Exception as e:
+             messages.warning(request, 'Got an error when trying to update a movie: {}'.format (e))
     return redirect('/')
 
 def delete(request, movie_id):
-    movie_name = AT.get(movie_id)['fields'].get('Name')
-    response = AT.delete(movie_id)
-    messages.warning(request, 'Deleted movie: {}'.format(movie_name))
+    try:
+        movie_name = AT.get(movie_id)['fields']
+        response=  AT.delete(movie_id)
+        messages.warning(request, 'Deleted movie: {}'.format(movie_name))
+    except Exception as e:
+             messages.warning(request, 'Got an error when trying to delete a movie: {}'.format (e))
+
+
+    #notify on delete
     return redirect('/')
+
+
+
+
+
+
+
